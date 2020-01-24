9cdf67b7584c4dddcc9c4770f5695ba62cd5a20c
vipksmosar <admin@npfx.ru>
2020-01-24 16:05:16 +0400

    Update create_project.py

--- create_project.py
+++ create_project.py
@@ -15,9 +15,9 @@ import argparse
 
 logging.basicConfig(format='%(name)s - %(levelname)s - %(message)s', level=logging.INFO)
  
-arg_parser = argparse.ArgumentParser(description='jira_create')
+arg_parser = argparse.ArgumentParser(description='wiki_publisher')
 arg_parser.add_argument('--jira_href', type=str, help="--jira_href 'https://jira.ru', default 'https://jira.ru'", default='https://jira.ru')
-arg_parser.add_argument('--filein', type=str, help="--filein 'C:/CSV/file.excel'")
+arg_parser.add_argument('--filein', type=str, help="--filein 'C:/CSV/HOSTS_VIRT_withScanned.csv'")
 arg_parser.add_argument('--LOGIN', type=str, help="--LOGIN ivan.ivanov")
 arg_parser.add_argument('--PASSWORD', type=str, help="--PASSWORD Pa$$w0rd")
 
@@ -31,8 +31,9 @@ class jira_creator:
         self.login = login
         self.password = password
         self.file = file
+        self.jira = None
         
-    def __prepare_DF (self, DF):
+    def __prepare_df (self, file):
         DF = pd.read_excel(file)
         list_decepen = []    
         DF = DF.rename({'date_start':'customfield_11610', 'date_end':'customfield_11630',\
@@ -61,64 +62,83 @@ class jira_creator:
     
     def __prepare_time (self, dict_issue):
         tasks_list = []
+        print(dict_issue)
+        print(type(dict_issue))
         if type(dict_issue) == dict:
             dict_issue['customfield_11610'] = re.search('\d{4}-\d{2}-\d{2}', str(dict_issue['customfield_11610']))[0]
             dict_issue['customfield_11630'] = re.search('\d{4}-\d{2}-\d{2}', str(dict_issue['customfield_11630']))[0]
             #print(dict_issue['customfield_11610'], dict_issue['customfield_11630'])
             return dict_issue
+        elif type(dict_issue) == list:
+            dict_issue=dict_issue[0]
+            dict_issue['customfield_11610'] = re.search('\d{4}-\d{2}-\d{2}', str(dict_issue['customfield_11610']))[0]
+            dict_issue['customfield_11630'] = re.search('\d{4}-\d{2}-\d{2}', str(dict_issue['customfield_11630']))[0]
         else:
-            raise ('Error type ', type(dict_issue))     
+            raise ('Error type '+str(type(dict_issue)))
     
     def __assign_update(self, key, assignee, manager_ia):
-        if self.jira == None:
-            self.jira = self.__jira_auth()
         issue = self.jira.issue(key)
         issue.update(fields={'assignee': {'name':assignee}})
         issue.update(fields={'customfield_21400': {'name':manager_ia}})
 
     def __project_mark_cell_to (self, key, project_id, mark_id):
-        if self.jira == None:
-            self.jira = self.__jira_auth()
         issue = self.jira.issue(key)
         issue.update(fields={'customfield_22700':[str(project_id)]})
         issue.update(fields={'customfield_22701':[str(mark_id)]})
 
-    def __create_project_issue (self, dict_project_issue):
-        if self.jira == None:
-            self.jira = self.__jira_auth()        
+    def __create_project_issue (self, dict_project_issue):   
         project_issue = self.jira.create_issue(fields=dict_project_issue)
         id_project = project_issue.id
         key_project = project_issue.key
         return key_project, id_project
     
     def __create_mark_issue (self, dict_mark_issue):
-        if self.jira == None:
-            self.jira = self.__jira_auth()
         mark_issue = self.jira.create_issue(fields=dict_mark_issue)
         id_mark_issue = mark_issue.id
         key_mark_issue = mark_issue.key
-        return id_mark_issue, key_mark_issue
+        return key_mark_issue, id_mark_issue
     
     def __create_task_issue (self, dict_task_issue):
-        if self.jira == None:
-            self.jira = self.__jira_auth()
         task_issue = self.jira.create_issue(fields=dict_task_issue)
         key_task_issue = task_issue.key
-        return key_task_issue
+        id_task_issue = task_issue.id
+        return key_task_issue, id_task_issue
         
     def __jira_auth (self):
         options = {"server": self.jira_href}
         self.jira = JIRA(options, basic_auth=(self.login, self.password))
         return self.jira
     
-    def create_project (self):
+    def __search_double_issue (self, dict_issue):
+        print(dict_issue)
+        print(dict_issue['summary'])
+        search_by_description = self.jira.search_issues('project='+'IAIT'+' and summary ~'+'"'+dict_issue['summary']+'"')
+        if search_by_description:
+            print(search_by_description)
+            return search_by_description[0].key, search_by_description[0].key
+        else:
+            if dict_issue['issuetype']['name']=='Проект':
+                key, id = self.__create_project_issue(dict_issue)
+                return key, id
+            elif dict_issue['issuetype']['name']=='Веха':
+                key, id = self.__create_project_issue(dict_issue)
+                return key, id
+            elif dict_issue['issuetype']['name']=='Задача':
+                key, id = self.__create_project_issue(dict_issue)
+                return key, id
+            else:
+                raise (dict_issue['issuetype']['name'], ' is unsupport name of issue')
+    
+    def IAIT_create_project (self):
+        if self.jira == None:
+            self.jira = self.__jira_auth()
         dict_scheme = {}
         DF, list_unique, manager_ia = self.__prepare_df(self.file)
         DF_project = DF[DF.hierarchy == list_unique[0]].drop(['hierarchy'], axis=1)
         dict_project= DF_project.to_dict('r')
         dict_project = self.__prepare_time(dict_project[0])
         assignee_issue = dict_project.pop('assignee')
-        project_key, project_id = self.__create_project_issue(dict_project)
+        project_key, project_id = self.__search_double_issue(dict_project)
         logging.info('{} is create'.format(project_key))
         print(dict_project)
         self.__assign_update(project_key,assignee_issue, manager_ia)
@@ -127,7 +147,7 @@ class jira_creator:
             dict_marks = DF_marks.iloc[:1,:].drop(['customfield_11627', 'customfield_11651', 'hierarchy'], axis=1).to_dict('r')
             dict_marks = self.__prepare_time(dict_marks[0])
             assignee_issue = dict_marks.pop('assignee')
-            mark_id, mark_key = self.__create_mark_issue(dict_marks)
+            mark_key, mark_id = self.__search_double_issue(dict_marks)
             logging.info('{} is create'.format(mark_key))
             print(dict_marks)
             self.__assign_update(mark_key ,assignee_issue, manager_ia)
@@ -140,16 +160,16 @@ class jira_creator:
             dict_tasks = DF_tasks.to_dict('r')
             for task in dict_tasks:
                 print(task)
-                dict_tasks = self.__prepare_time(dict_tasks)
+                dict_tasks = self.__prepare_time(task)
                 assignee_issue = task.pop('assignee')
                 print(task)
-                task_key = self.__create_task_issue(task)
+                task_key = self.__search_double_issue(task)
                 logging.info('{} is create'.format(task_key))
                 self.__assign_update(task_key ,assignee_issue, manager_ia)
-                self.jira.create_issue_link(type="Иерархия задач", inwardIssue=mark_key, outwardIssue=task_key)
+                #self.jira.create_issue_link(type="Иерархия задач", inwardIssue=mark_key, outwardIssue=task_key)
                 logging.info('{} is create link to'.format(task_key, mark_key))
             
         
     
 create_tasks = jira_creator(args.jira_href, args.LOGIN, args.PASSWORD, args.filein)
-create_tasks.create_project()
+create_tasks.IAIT_create_project()
